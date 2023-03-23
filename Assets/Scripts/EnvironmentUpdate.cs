1276a09e453ad3e18966b69b2a54e97c5fa5e18c
yzs020220 <754052428@qq.com>
2023-03-23 12:31:00 +0800

    third commit

--- Assets/Scripts/EnvironmentUpdate.cs
+++ Assets/Scripts/EnvironmentUpdate.cs
@@ -7,10 +7,10 @@ public class EnvironmentUpdate : MonoBehaviour
 {
     public Walkable cube0;
     public Walkable cube1;
-    public Walkable stair0;
-    public Walkable stair1;
-    public Walkable cube2;
-    public Walkable stair2;
+    // public Walkable stair0;
+    // public Walkable stair1;
+    // public Walkable cube2;
+    // public Walkable stair2;
 
     public Walkable[] platform;
     public bool pathFirstSet = false;
@@ -18,8 +18,8 @@ public class EnvironmentUpdate : MonoBehaviour
     public CameraManager cameraManager;
 
     private float _dist0;
-    private float _dist1;
-    private float _dist2;
+    // private float _dist1;
+    // private float _dist2;
     private float _platformDist;
 
     // Start is called before the first frame update
@@ -57,8 +57,8 @@ public class EnvironmentUpdate : MonoBehaviour
     void PathDistSet()
     {
         _dist0 = (cube0.screenPoint - cube1.screenPoint).magnitude;
-        _dist1 = (stair0.screenPoint - stair1.screenPoint).magnitude;
-        _dist2 = (cube2.screenPoint - stair2.screenPoint).magnitude;
+        // _dist1 = (stair0.screenPoint - stair1.screenPoint).magnitude;
+        // _dist2 = (cube2.screenPoint - stair2.screenPoint).magnitude;
         // Debug.Log(_dist0);
         // Debug.Log(_dist1);
         // Debug.Log(_dist2);
@@ -70,12 +70,13 @@ public class EnvironmentUpdate : MonoBehaviour
         {
             for (int j = i + 1; j < platform.Length; j++)
             {
+                platform[i].GetScreenPoint();
+                platform[j].GetScreenPoint();
                 Vector2 iScreenPos = new Vector2(platform[i].screenPoint.x, platform[i].screenPoint.y);
                 Vector2 jScreenPos = new Vector2(platform[j].screenPoint.x, platform[j].screenPoint.y);
                 _platformDist = (iScreenPos - jScreenPos).magnitude;
 
-                if (Mathf.Abs(_platformDist - _dist0) < .5f || Mathf.Abs(_platformDist - _dist1) < .5f ||
-                    Mathf.Abs(_platformDist - _dist2) < .5f)
+                if (Mathf.Abs(_platformDist - _dist0) < .5f)
                 {
                     WalkPath walkPath0 = new WalkPath();
                     WalkPath walkPath1 = new WalkPath();
